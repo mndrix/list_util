26b8e8cc9202e1c978d91a8b0f107ac6a3e4df5a
Michael Hendricks <michael@ndrix.org>
2013-08-30 10:42:32 -0600

    Release v0.1.2
    I moved some README files around to support the latest conventions for
    pack files.

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(list_util).
-version('0.1.1').
+version('0.1.2').
 title('Predicates for working with lists').
 keywords([lists]).
 author( 'Michael Hendricks', 'michael@ndrix.org' ).
 packager( 'Michael Hendricks', 'michael@ndrix.org' ).
 maintainer( 'Michael Hendricks', 'michael@ndrix.org' ).
 home('http://packs.ndrix.com/list_util/index.html').
-download('http://packs.ndrix.com/list_util/list_util-0.1.1.tgz').
+download('http://packs.ndrix.com/list_util/list_util-0.1.2.tgz').
