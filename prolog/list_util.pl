5add2143663feb7f4d3a9948bb6de6264a982c4d
Michael Hendricks <michael@ndrix.org>
2014-04-30 09:01:21 -0600

    Implement lines/2
    It's often convenient to work with a file as if it were just a list of
    lines.  However, we don't want to load the entire file into memory.
    
    Unfortunately, this uses the same stream repositioning trick used by
    library(pure_input) so it only works on certain kinds of streams.

--- prolog/list_util.pl
+++ prolog/list_util.pl
@@ -1,6 +1,7 @@
 :- module(list_util,
           [ drop/3
           , keysort_r/2
+          , lines/2
           , map_include/3
           , maximum/2
           , maximum_by/3
@@ -13,6 +14,7 @@
           , take/3
           , xfy_list/3
           ]).
+:- use_module(library(readutil), [read_line_to_string/2]).
 :- use_module(library(when), [when/2]).
 
 %%  split(?Combined:list, ?Separator, ?Separated:list(list)) is det.
@@ -233,6 +235,38 @@ iterate_(Goal, State0, List) :-
         List = []
     ).
 
+
+%% lines(+Source, -Lines:list(string)) is det.
+%
+%  Lines is a lazy list of lines from Source. Source can be
+%  one of:
+%
+%    * file(Filename) - read lines from a file
+%    * stream(Stream) - read lines from a stream
+%
+% After the last line has been read, all relevant streams are
+% automatically closed. One must be able to call set_stream_position/2
+% on Stream.
+%
+% Each line in Lines does not contain the line terminator.
+lines(file(Filename), Lines) :-
+    open(Filename, read, Stream, []),
+    lines(stream(Stream), Lines).
+lines(stream(Stream), Lines) :-
+    stream_property(Stream, position(Pos)),
+    iterate(lines_, Stream-Pos, Lines).
+
+lines_(Stream-Pos0, Stream-Pos, Line) :-
+    is_stream(Stream), % don't touch a closed stream
+    set_stream_position(Stream, Pos0), % to handle backtracking
+    read_line_to_string(Stream, Tentative),
+    ( Tentative = end_of_file ->
+        close(Stream),
+        fail  % to end the list of lines
+    ; % there was a line ->
+        stream_property(Stream, position(Pos)),
+        Line = Tentative
+    ).
 
 %%	sort_r(+List:list, -ReverseSorted:list) is det.
 %
---
+++ t/lines.pl
@@ -0,0 +1,26 @@
+:- use_module(library(list_util)).
+
+sample(Lines) :-
+    lines(file("t/lines.txt"), Lines).
+
+:- use_module(library(tap)).
+
+
+length :-
+    sample(Lines),
+    once(length(Lines, Len)),  % built in length leaves choicepoints for lazy lists
+    Len == 4.
+
+content :-
+    sample(Lines),
+    Lines = [A,B,C,D],
+    A == "alpha",
+    B == "beta",
+    C == "gamma",
+    D == "delta".
+
+partial :-
+    sample(Lines),
+    Lines = [Head|Rest],
+    Head == "alpha",
+    var(Rest).
