87e9ba62a3241bb166f4b26c15c29822257a0795
Michael Hendricks <michael@ndrix.org>
2014-05-06 15:05:05 -0600

    Add tests for sort_by/3 (omitted earlier)

---
+++ t/sort_by.pl
@@ -0,0 +1,26 @@
+:- use_module(library(list_util)).
+:- use_module(library(apply), [maplist/3]).
+:- use_module(library(record)).
+
+:- record person(name:string, age:positive_integer).
+
+people([ person("John", 27)
+       , person("Tom", 30)
+       , person("Sue", 18)
+       , person("Will", 63)
+       ]).
+
+
+:- use_module(library(tap)).
+
+by_name :-
+    people(People),
+    sort_by(person_name, People, Sorted),
+    maplist(person_name, Sorted, Names),
+    Names == ["John","Sue","Tom","Will"].
+
+by_age :-
+    people(People),
+    sort_by(person_age, People, Sorted),
+    maplist(person_age, Sorted, Ages),
+    Ages == [18, 27, 30, 63].
