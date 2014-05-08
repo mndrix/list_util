2c8871d173a40484806366802f0b606181663961
Michael Hendricks <michael@ndrix.org>
2014-05-08 14:19:39 -0600

    Move lines/2 implementation to a separate file
    The implementation will soon grow.  I want to make it clear which
    predicates are in the supporting cast and which are meant for public
    consumption.  I may refactor again to use a module.

--- prolog/list_util.pl
+++ prolog/list_util.pl
@@ -22,6 +22,8 @@
 :- use_module(library(readutil), [read_line_to_string/2]).
 :- use_module(library(when), [when/2]).
 
+:- include(lines).
+
 % TODO look through list library of Amzi! Prolog for ideas: http://www.amzi.com/manuals/amzi/libs/list.htm
 % TODO look through ECLiPSe list library: http://www.eclipseclp.org/doc/bips/lib/lists/index.html
 
@@ -243,39 +245,6 @@ iterate_(Goal, State0, List) :-
         iterate(Goal, State, Xs)
     ; % goal failed, list is done ->
         List = []
-    ).
-
-
-%% lines(+Source, -Lines:list(string)) is det.
-%
-%  Lines is a lazy list of lines from Source. Source can be
-%  one of:
-%
-%    * file(Filename) - read lines from a file
-%    * stream(Stream) - read lines from a stream
-%
-% After the last line has been read, all relevant streams are
-% automatically closed. One must be able to call set_stream_position/2
-% on Stream.
-%
-% Each line in Lines does not contain the line terminator.
-lines(file(Filename), Lines) :-
-    open(Filename, read, Stream, []),
-    lines(stream(Stream), Lines).
-lines(stream(Stream), Lines) :-
-    stream_property(Stream, position(Pos)),
-    iterate(lines_, Stream-Pos, Lines).
-
-lines_(Stream-Pos0, Stream-Pos, Line) :-
-    is_stream(Stream), % don't touch a closed stream
-    set_stream_position(Stream, Pos0), % to handle backtracking
-    read_line_to_string(Stream, Tentative),
-    ( Tentative = end_of_file ->
-        close(Stream),
-        fail  % to end the list of lines
-    ; % there was a line ->
-        stream_property(Stream, position(Pos)),
-        Line = Tentative
     ).
 
 %% positive_integers(-List:list(positive_integer)) is det.
