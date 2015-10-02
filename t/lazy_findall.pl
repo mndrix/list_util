fda6175f946fc26e7bd03e350b33932ffdcad0b0
Michael Hendricks <michael@ndrix.org>
2015-10-02 09:17:31 -0600

    lazy_findall/3: test for thread GC
    Ideally lazy_findall/3 would perfectly garbage collect its worker
    threads.  In a few test cases, that's already true.  I added those
    checks to the tests to make sure we don't regress here.
    
    When we don't consume all solutions, a worker thread is still left lying
    around.  I created a TODO test to remind us of this problem.

--- t/lazy_findall.pl
+++ t/lazy_findall.pl
@@ -17,30 +17,48 @@ eventually_deadly.
 eventually_deadly :-
     throw(die).
 
+% number of current Prolog threads
+thread_count(N) :-
+    bagof(x,T^Status^thread_property(T,status(Status)),Xs),
+    length(Xs,N).
+
 :- use_module(library(tap)).
 
 no_solutions :-
+    thread_count(N),
     lazy_findall(x,fail,Xs),
-    Xs=[].
+    Xs=[],
+    thread_count(N).
 
 take_finite :-
+    thread_count(N),
     lazy_findall(X,bob(X),Bob),
     take(30,Bob,Letters),
-    Letters = [b,o,b].
+    Letters = [b,o,b],
+    thread_count(N).
 
 take_infinite :-
     lazy_findall(X,foo(X),Xs),
     take(4,Xs,L),
     L == [x,y,z,x].
 
+take_infinite_gc(todo) :-
+    thread_count(N),
+    take_infinite,  % shouldn't leave threads lying about
+    thread_count(N).
+
 % missing predicate fails right away since first value is eager
 'no predicate: lazy'(throws(_)) :-
-    lazy_findall(x,no_predicate_with_this_name,_).
+    thread_count(N),
+    lazy_findall(x,no_predicate_with_this_name,_),
+    thread_count(N).
 
 'delayed exception: once is ok' :-
     lazy_findall(x,eventually_deadly,Xs),
     Xs = [_|_].
 
 'delayed exception: twice dies'(throws(die)) :-
+    thread_count(N),
     lazy_findall(x,eventually_deadly,Xs),
-    Xs = [_,_|_].
+    Xs = [_,_|_],
+    thread_count(N).
