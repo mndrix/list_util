20a63f5e13eb83ba63afbb65a47e5dd9cd163716
Michael Hendricks <michael@ndrix.org>
2016-06-29 09:12:00 -0400

    Add tests for {minimum,maximum}_{by,with}

--- t/maximum.pl
+++ t/maximum.pl
@@ -1,5 +1,22 @@
 :- use_module(library(list_util)).
+:- use_module(library(record)).
+
+:- record person(name:string, age:positive_integer).
+
+people([ person("John", 27)
+       , person("Tom", 30)
+       , person("Sue", 18)
+       , person("Will", 63)
+       ]).
+
+% compare people by age
+age(Order, A, B) :-
+    person_age(A, AgeA),
+    person_age(B, AgeB),
+    compare(Order, AgeA, AgeB).
+
 :- use_module(library(tap)).
+
 
 atoms :-
     maximum([c,b,a,d], d).
@@ -15,3 +32,13 @@ empty(fail) :-
     maximum(List, Max),
     Max = z,
     List = [z, k, r, t, v, u, j].
+
+'maximum by age' :-
+    people(People),
+    maximum_by(age, People, Person),
+    Person == person("Will", 63).
+
+'maximum with name' :-
+    people(People),
+    maximum_with(person_name, People, Person),
+    Person == person("Will", 63).
--- t/minimum.pl
+++ t/minimum.pl
@@ -1,4 +1,20 @@
 :- use_module(library(list_util)).
+:- use_module(library(record)).
+
+:- record person(name:string, age:positive_integer).
+
+people([ person("John", 27)
+       , person("Tom", 30)
+       , person("Sue", 18)
+       , person("Will", 63)
+       ]).
+
+% compare people by age
+age(Order, A, B) :-
+    person_age(A, AgeA),
+    person_age(B, AgeB),
+    compare(Order, AgeA, AgeB).
+
 :- use_module(library(tap)).
 
 atoms :-
@@ -15,3 +31,13 @@ empty(fail) :-
     minimum(List, Minimum),
     Minimum = j,
     List = [z, k, r, t, v, u, j].
+
+'minimum by age' :-
+    people(People),
+    minimum_by(age, People, Person),
+    Person == person("Sue", 18).
+
+'minimum with name' :-
+    people(People),
+    minimum_with(person_name, People, Person),
+    Person == person("John", 27).
